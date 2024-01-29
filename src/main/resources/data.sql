224aada3462ec17b8cd15c2cded901faee6fba44
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-01-29 14:39:23 -0300

    Correção de quantidade no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
-insert ignore into dish (id, created_time, name, price, updated_time) values (1, now(), 'Faláfel', 10.00, now());
-insert ignore into dish (id, created_time, name, price, updated_time) values (2, now(), 'Peixe', 10.00, now());
-insert ignore into dish (id, created_time, name, price, updated_time) values (3, now(), 'Gaspacho', 10.00, now());
-insert ignore into dish (id, created_time, name, price, updated_time) values (4, now(), 'Ramen', 10.00, now());
-insert ignore into dish (id, created_time, name, price, updated_time) values (5, now(), 'Macarrão Rigatoni', 10.00, now());
-insert ignore into dish (id, created_time, name, price, updated_time) values (6, now(), 'Hambúrguer', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (1, now(), 'Faláfel', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (2, now(), 'Peixe', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (3, now(), 'Gaspacho', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (4, now(), 'Ramen', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (5, now(), 'Macarrão Rigatoni', 10.00, now());
+insert ignore into tb_dish (id, created_time, name, price, updated_time) values (6, now(), 'Hambúrguer', 10.00, now());
 
-insert ignore into drink (id, created_time, name, price, updated_time) values (1, now(), 'Margarita', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (2, now(), 'Cosmopolitan', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (3, now(), 'Daiquiri', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (4, now(), 'Gim', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (5, now(), 'Manhattan', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (6, now(), 'Negroni', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (7, now(), 'Old Fashioned', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (8, now(), 'Expresso Martini', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (9, now(), 'Martini de Maracujá', 20.0, now());
-insert ignore into drink (id, created_time, name, price, updated_time) values (10, now(), 'Mimosa', 20.0, now());
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (1, now(), 'Margarita', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (2, now(), 'Cosmopolitan', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (3, now(), 'Daiquiri', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (4, now(), 'Gim', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (5, now(), 'Manhattan', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (6, now(), 'Negroni', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (7, now(), 'Old Fashioned', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (8, now(), 'Expresso Martini', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (9, now(), 'Martini de Maracujá', 20.0, now(), 1);
+insert ignore into drink (id, created_time, name, price, updated_time, quantity) values (10, now(), 'Mimosa', 20.0, now(), 1);
 
 insert ignore into dessert (id, created_time, name, price, updated_time) values (1, now(), 'Bolo', 20.0, now());
 insert ignore into dessert (id, created_time, name, price, updated_time) values (2, now(), 'Cookie', 20.0, now());
