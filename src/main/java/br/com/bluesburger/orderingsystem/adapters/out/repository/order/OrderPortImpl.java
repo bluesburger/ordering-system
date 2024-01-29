bcb8fb3dfc3b0ce325515fb5f635d95ecd4be91c
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-01-29 15:59:48 -0300

    Correção na busca de orders por usuário, e inicialização de dados

--- src/main/java/br/com/bluesburger/orderingsystem/adapters/out/repository/order/OrderPortImpl.java
+++ src/main/java/br/com/bluesburger/orderingsystem/adapters/out/repository/order/OrderPortImpl.java
@@ -1,25 +1,30 @@
 package br.com.bluesburger.orderingsystem.adapters.out.repository.order;
 
 
-import br.com.bluesburger.orderingsystem.adapters.out.exceptions.OrderNotFoundException;
-import br.com.bluesburger.orderingsystem.adapters.out.repository.order.entities.OrderEntity;
-import br.com.bluesburger.orderingsystem.adapters.out.repository.order.mapper.OrderMapper;
-import br.com.bluesburger.orderingsystem.core.domain.Order;
-import br.com.bluesburger.orderingsystem.core.domain.OrderStatus;
-import br.com.bluesburger.orderingsystem.core.domain.User;
-import br.com.bluesburger.orderingsystem.ports.out.OrderPort;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import static br.com.bluesburger.orderingsystem.adapters.out.repository.order.mapper.OrderMapper.mapperOrderToOrderEntity;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static br.com.bluesburger.orderingsystem.adapters.out.repository.order.mapper.OrderMapper.mapperOrderToOrderEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.bluesburger.orderingsystem.adapters.out.exceptions.OrderNotFoundException;
+import br.com.bluesburger.orderingsystem.adapters.out.repository.order.entities.OrderEntity;
+import br.com.bluesburger.orderingsystem.adapters.out.repository.order.mapper.OrderMapper;
+import br.com.bluesburger.orderingsystem.adapters.out.repository.user.UserRepository;
+import br.com.bluesburger.orderingsystem.core.domain.Order;
+import br.com.bluesburger.orderingsystem.core.domain.OrderStatus;
+import br.com.bluesburger.orderingsystem.core.domain.User;
+import br.com.bluesburger.orderingsystem.ports.out.OrderPort;
 
 @Component
 @Transactional
 public class OrderPortImpl implements OrderPort {
+	
+	@Autowired
+	private UserRepository userRepository;
 
     @Autowired
     private OrderRepository orderRepository;
@@ -29,7 +34,8 @@ public class OrderPortImpl implements OrderPort {
 
     @Override
     public List<Order> getOrdersByUser(User user) {
-        var orderEntityList = orderRepository.findAllByUserEntity(user);
+    	var userEntity = userRepository.getUserByCpf(user.getCpf());
+        var orderEntityList = orderRepository.findAllByUserEntity(userEntity);
 
         return orderEntityList.stream()
                 .map(OrderMapper::mapperOrderToOrderEntity)
--- src/main/java/br/com/bluesburger/orderingsystem/adapters/out/repository/order/OrderRepository.java
+++ src/main/java/br/com/bluesburger/orderingsystem/adapters/out/repository/order/OrderRepository.java
@@ -1,18 +1,19 @@
 package br.com.bluesburger.orderingsystem.adapters.out.repository.order;
 
-import br.com.bluesburger.orderingsystem.adapters.out.repository.order.entities.OrderEntity;
-import br.com.bluesburger.orderingsystem.core.domain.OrderStatus;
-import br.com.bluesburger.orderingsystem.core.domain.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import br.com.bluesburger.orderingsystem.adapters.out.repository.order.entities.OrderEntity;
+import br.com.bluesburger.orderingsystem.adapters.out.repository.user.entities.UserEntity;
+import br.com.bluesburger.orderingsystem.core.domain.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
     List<OrderEntity> findAllByStatus(OrderStatus status);
 
-    List<OrderEntity> findAllByUserEntity(User user);
+    List<OrderEntity> findAllByUserEntity(UserEntity user);
 
     @Query("SELECT o FROM tb_order o WHERE o.status <> 'PEDIDO_FINALIZADO' ORDER BY CASE o.status " +
             "WHEN 'PEDIDO_PRONTO' THEN 1 " +
