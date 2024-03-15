f779e24ab4b39b740aa9fef99f3d83e0eed7e32e
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-15 19:44:58 -0300

    Second attempt to fix ecr-push.tf

--- terraform/ecr-push.tf
+++ terraform/ecr-push.tf
@@ -1,6 +1,6 @@
 # Definição de um recurso de execução local para fazer o push da imagem
 resource "null_resource" "push_image_to_ecr" {
   provisioner "local-exec" {
-    command = "aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com && docker build -t ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/ordering-systems3:latest . && docker push ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/ordering-systems3:latest"
+    command = "aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com && docker build -t ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/ordering-systems3:latest ..\ && docker push ${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/ordering-systems3:latest"
   }
 }
