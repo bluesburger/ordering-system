4315ac2f61769dad3ea36da641e0d3403104c9cc
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-15 19:22:02 -0300

    Add region e account_id variables to terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -31,3 +31,11 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AWS_ACCOUNT_ID" {
+  type = string
+}
