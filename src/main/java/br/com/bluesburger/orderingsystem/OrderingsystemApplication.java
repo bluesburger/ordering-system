632f56a2c13d978ebe66a81b89d01e68b8587800
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-15 19:50:16 -0300

    Teste de ecr

--- src/main/java/br/com/bluesburger/orderingsystem/OrderingsystemApplication.java
+++ src/main/java/br/com/bluesburger/orderingsystem/OrderingsystemApplication.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-/** */
+/** 
+ * 
+*/
 @SpringBootApplication
 @Controller
 public class OrderingsystemApplication {
