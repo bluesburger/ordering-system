24a7b0c759606cc4e207e2086a1f0e5f1797380f
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-14 22:07:55 -0300

    Add comment

--- src/main/java/br/com/bluesburger/orderingsystem/OrderingsystemApplication.java
+++ src/main/java/br/com/bluesburger/orderingsystem/OrderingsystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/** */
 @SpringBootApplication
 @Controller
 public class OrderingsystemApplication {
