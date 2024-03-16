0b9280fd86c4a099875b0f18dc5eb5511b190e42
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-16 03:22:47 -0300

    Correção de nome de repositório para deixar compatível com padrão determinado.

--- terraform/ecr-push.tf
+++ terraform/ecr-push.tf
@@ -1,6 +1,6 @@
 # Definição de um recurso de execução local para fazer o push da imagem
 resource "null_resource" "push_image_to_ecr" {
   provisioner "local-exec" {
-    command = "aws ecr get-login-password --region ${var.defaultRegion} | docker login --username AWS --password-stdin ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com && docker build -t ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com/ordering-system:latest ../ && docker push ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com/ordering-system:latest"
+    command = "aws ecr get-login-password --region ${var.defaultRegion} | docker login --username AWS --password-stdin ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com && docker build -t ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com/ordering-systems3:latest ../ && docker push ${var.accountId}.dkr.ecr.${var.defaultRegion}.amazonaws.com/ordering-systems3:latest"
   }
 }
