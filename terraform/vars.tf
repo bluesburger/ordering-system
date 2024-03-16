366b23bba43378bcfe458df369ffa709d85a3c1e
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-03-15 22:14:18 -0300

    Terraform fixes

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,26 +2,12 @@ variable "projectName" {
   default = "bluesburguer"
 }
 
-variable "regionDefault" {
-  default = "us-east-1"
-}
-
 variable "tags" {
   type = map(string)
   default = {
     App      = "bluesburguer",
     Ambiente = "Desenvolvimento"
   }
-}
-
-# Define a variável com o ID da conta da AWS
-variable "aws_account_id" {
-  default = "637423186279"
-}
-
-# Define a variável com a região da AWS
-variable "aws_region" {
-  default = "us-east-1"
 }
 
 variable "AWS_ACCESS_KEY_ID" {
