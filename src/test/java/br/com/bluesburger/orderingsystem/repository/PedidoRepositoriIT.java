8db4c8c2f6eb686fadce14cd24ce0e10957599ca
fernandomqs <fernandopozomqs@gmail.com>
2024-04-12 17:58:03 -0300

    teste de integração

--- src/test/java/br/com/bluesburger/orderingsystem/repository/PedidoRepositoriIT.java
+++ src/test/java/br/com/bluesburger/orderingsystem/repository/PedidoRepositoriIT.java
@@ -5,11 +5,16 @@ import br.com.bluesburger.orderingsystem.adapters.out.repository.dish.entities.O
 import br.com.bluesburger.orderingsystem.adapters.out.repository.drink.entities.OrderItemDrinkEntity;
 import br.com.bluesburger.orderingsystem.adapters.out.repository.order.OrderRepository;
 import br.com.bluesburger.orderingsystem.adapters.out.repository.order.entities.OrderEntity;
+import br.com.bluesburger.orderingsystem.core.domain.OrderItemDessert;
+import br.com.bluesburger.orderingsystem.core.domain.OrderItemDish;
+import br.com.bluesburger.orderingsystem.core.domain.OrderItemDrink;
 import br.com.bluesburger.orderingsystem.core.domain.OrderStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -18,6 +23,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 
@@ -27,88 +33,74 @@ import static org.mockito.Mockito.times;
 public class PedidoRepositoriIT {
 
 
-//    @Autowired
-//    private OrderRepository pedidoRepository;
-//
-//    @Test
-//    void deveCriarTabela(){
-//        var totalDeRegistros = pedidoRepository.count();
-//        assertThat(totalDeRegistros).isNotNegative();
-//    }
-//
-//    @Test
-//    void deveRegistrarPedido(){
-//        //arrange
-//        var pedido = gerarPedido();
-//
-//        //Act
-//        var pedidoArmazenado = registrarPedido(pedido);
-//
-//        //Assert
-//        assertThat(pedidoArmazenado)
-//                .isInstanceOf(OrderEntity.class)
-//                .isNotNull();
-//        assertThat(pedidoArmazenado.getId()).isEqualTo(pedido.getId());
-//
-//    }
-//
-//    @Test
-//    void deveBuscarPedido(){
-//        //Arrange
-//        var pedido = gerarPedido();
-//        registrarPedido(pedido);
-//
-//        //Act
-//        var pedidoArmazenadoOptional = pedidoRepository.findById(pedido.getId());
-//
-//        // Assert
-//        assertThat(pedidoArmazenadoOptional)
-//                .isPresent();
-//
-//        pedidoArmazenadoOptional.ifPresent(pedidoArmazenado -> {
-//            assertThat(pedidoArmazenado.getId()).isEqualTo(pedido.getId());
-//        });
-//
-//    }
-//
-//    @Test
-//    void deveRemoverPedido(){
-//        //Arrange
-//        var pedido = gerarPedido();
-//        registrarPedido(pedido);
-//
-//        //Act
-//        pedidoRepository.deleteById(pedido.getId());
-//        var pedidoRecebidoOptional = pedidoRepository.findById(pedido.getId());
-//
-//        //Assert
-//        assertThat(pedidoRecebidoOptional).isEmpty();
-//    }
-//
-//
-//    private OrderEntity gerarPedido(){
-//
-//        OrderItemDishEntity dishEntity1 = mock(OrderItemDishEntity.class);
-//        OrderItemDishEntity dishEntity2 = mock(OrderItemDishEntity.class);
-//
-//        OrderItemDessertEntity dessertEntity1 = mock(OrderItemDessertEntity.class);
-//        OrderItemDessertEntity dessertEntity2 = mock(OrderItemDessertEntity.class);
-//
-//        OrderItemDrinkEntity drinkEntity1 = mock(OrderItemDrinkEntity.class);
-//        OrderItemDrinkEntity drinkEntity2 = mock(OrderItemDrinkEntity.class);
-//
-//        return OrderEntity.builder()
-//                .id(1L)
-//                .status(OrderStatus.PEDIDO_RECEBIDO)
-//                .totalValue(BigDecimal.ZERO)
-//                .dishEntities(List.of(dishEntity1, dishEntity2))
-//                .dessertEntities(List.of(dessertEntity1, dessertEntity2))
-//                .drinkEntities(List.of(drinkEntity1, drinkEntity2))
-//                .createdTime(LocalDateTime.now())
-//                .build();
-//    }
-//
-//    private OrderEntity registrarPedido(OrderEntity pedido){
-//        return pedidoRepository.save(pedido);
-//    }
+    @Autowired
+    private OrderRepository pedidoRepository;
+
+    @BeforeEach
+
+    @Test
+    void deveCriarTabela(){
+        var totalDeRegistros = pedidoRepository.count();
+        assertThat(totalDeRegistros).isNotNegative();
+    }
+
+    @Test
+    //@Sql("/data.sql")
+    void deveRegistrarPedido(){
+        //arrange
+        var pedido = gerarPedido();
+
+        //Act
+        var pedidoArmazenado = registrarPedido(pedido);
+
+        //Assert
+        assertThat(pedidoArmazenado)
+                .isInstanceOf(OrderEntity.class)
+                .isNotNull();
+        assertThat(pedidoArmazenado.getId()).isEqualTo(pedido.getId());
+
+    }
+
+    @Test
+    void deveBuscarPedido(){
+        //Arrange
+        var pedido = gerarPedido();
+        registrarPedido(pedido);
+
+        //Act
+        var pedidoArmazenadoOptional = pedidoRepository.findById(pedido.getId());
+
+        // Assert
+        assertThat(pedidoArmazenadoOptional)
+                .isPresent();
+
+        pedidoArmazenadoOptional.ifPresent(pedidoArmazenado -> {
+            assertThat(pedidoArmazenado.getId()).isEqualTo(pedido.getId());
+        });
+
+    }
+
+    @Test
+    void deveRemoverPedido(){
+        //Arrange
+        var pedido = gerarPedido();
+        registrarPedido(pedido);
+
+        //Act
+        pedidoRepository.deleteById(pedido.getId());
+        var pedidoRecebidoOptional = pedidoRepository.findById(pedido.getId());
+
+        //Assert
+        assertThat(pedidoRecebidoOptional).isEmpty();
+    }
+
+
+    private OrderEntity gerarPedido(){
+        List<OrderEntity> pedidoEntities = pedidoRepository.findAll();
+        return pedidoEntities.isEmpty() ? null : pedidoEntities.get(0);
+    }
+
+    private OrderEntity registrarPedido(OrderEntity pedido){
+        return pedidoRepository.save(pedido);
+    }
 }
